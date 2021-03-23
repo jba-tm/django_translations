a3371a6bcb29be854262588657d87421342146de
jba-tm <jumageldiyewb@list.ru>
2021-03-23 23:06:05 +0500

    Fix requirements

--- django_translations/apps/translation/models.py
+++ django_translations/apps/translation/models.py
@@ -21,20 +21,6 @@ class Post(TranslatableModel):
     def __str__(self):
         return self.slug
 
-    # def get_fallback_languages(self):
-    #     return []
-
-    def get_translation(self, language_code, related_name=None):
-        """
-        Fetch the translated model
-        """
-        print('get')
-        meta = self._parler_meta._get_extension_by_related_name(related_name)
-        return self._get_translated_model(language_code, meta=meta, auto_create=True)
-
-    def safe_translation_getter(self, *args, **kwargs):
-        return super().safe_translation_getter(*args, **kwargs)
-
 
 class PostTranslation(TranslatedFieldsModel):
     master = models.ForeignKey(Post, related_name='translations', on_delete=models.CASCADE)
